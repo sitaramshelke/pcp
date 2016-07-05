05bb549f5274682abf6d5b0f6f40e919c3f99e95
sitaramshelke <sitaramshelke@gmail.com>
2016-07-05 16:59:40 +0530

    Add copyright header

--- src/pcp/pidstat/test/none_handler_printer_decorator_test.py
+++ src/pcp/pidstat/test/none_handler_printer_decorator_test.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env pmpython
+#
+# Copyright (C) 2016 Sitaram Shelke.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# pylint: disable=C0103,R0914,R0902import unittest
+
 import unittest
 from mock import Mock
 from pcp_pidstat import NoneHandlingPrinterDecorator
 class TestNoneHandlingPrinterDecorator(unittest.TestCase):
-    
+
     def test_print_report_without_none_values(self):
         printer = Mock()
         printer.Print = Mock()
